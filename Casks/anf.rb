26c440c6452752112e38b6308dcdcbc90182b691
Seongil Park <zihado@gmail.com>
2026-06-13 05:34:48 +0900

    anf 1.1.4

--- Casks/anf.rb
+++ Casks/anf.rb
@@ -1,6 +1,6 @@
 cask "anf" do
-  version "1.1.3"
-  sha256 "ec9d4d0aa1baa4b57642f9ec2b065c680d521877219009759cee77c1d7d5af8d"
+  version "1.1.4"
+  sha256 "f81722de0221b658948136d984d0fdb7d7a1488dd976200f21c35f960868bbb6"
 
   url "https://github.com/rescenedev/anf/releases/download/v#{version}/anf.zip"
   name "anf"
